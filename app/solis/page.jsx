5eec72d75636573932004cd9efc183754be9a8da
bob <saymer98@gmail.com>
2023-07-27 13:23:51 +0200

    tes

--- app/solis/page.jsx
+++ app/solis/page.jsx
@@ -12,8 +12,7 @@ const Loading = () => {
   return (
     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-
-      <meshStandardMaterial attach="material" color="white" transparent opacity={0.6} />
+      <meshBasicMaterial  color="orange" transparent opacity={0.9} />
     </mesh>
   );
 };
